5ffea944d0d0bc82bb11832d26a13a70c81d3ca4
JordanJStone <statictrack@gmail.com>
2021-01-18 16:41:03 -0700

    Completed looping, fixed error in ComputedProp

--- src/components/ComputedPropertiesExercise.vue
+++ src/components/ComputedPropertiesExercise.vue
@@ -15,24 +15,24 @@
 
 
 <script>
-import { computed, reactive } from "vue";
-export default {
-  name: "computed-properties-exercise",
-  setup() {
-    const state = reactive;
-    ({
-      // After getting this to work, challenge yourself to add an input field and bind message to it, so as you type the messag gets reversed!
-      message: "Hello World!",
-      reversedMessage: computed(() => {
-        // all computed properties must return a value, what do you want this one to return?
-        return ""
-      }),
-    });
-    return {
-      state,
-    };
-  },
-};
+  import { computed, reactive } from "vue";
+  export default {
+    name: "computed-properties-exercise",
+    setup() {
+      const state = reactive
+        ({
+          // After getting this to work, challenge yourself to add an input field and bind message to it, so as you type the messag gets reversed!
+          message: "Hello World!",
+          reversedMessage: computed(() => {
+            // all computed properties must return a value, what do you want this one to return?
+            return ""
+          }),
+        });
+      return {
+        state,
+      };
+    },
+  };
 </script>
 
 
